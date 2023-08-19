368b7148c570e0ab4bfcbb59828bc44ef6a00ba0
Betül <94698551+betulsimsek@users.noreply.github.com>
2023-08-19 16:17:10 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, {  useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Line } from 'react-chartjs-2';
+import { Chart, LineElement, PointElement, LinearScale, CategoryScale, Title } from 'chart.js/auto';
+
+// Your chart configuration code here
+
+// Register the elements and scales before using them
+Chart.register(LineElement, PointElement, LinearScale, CategoryScale, Title);
+
+// Your chart configuration code here
+
 
 function App() {
   const [orders, setOrders] = useState([]);
   const [vendorName, setVendorName] = useState('');
-
-  const fetchOrders = async () => {
-    try {
-      const response = await axios.get(`http://localhost:5000/orders?vendorName=${vendorName}`);
-      const modifiedOrders = response.data.map((order) => ({
-        ...order}));
-      const groupedOrders = groupOrders(modifiedOrders);
-      setOrders(groupedOrders);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [chartData, setChartData] = useState(null);
+  const [chartOptions, setChartOptions] = useState(null);
 
   const groupOrders = (orders) => {
     const groupedOrders = orders.reduce((acc, order) => {
-      const existingOrder = acc.find((groupedOrder) => 
-        groupedOrder.product_id === order.product_id && 
-        groupedOrder.vendor_name === order.vendor_name
+      const existingOrder = acc.find(
+        (groupedOrder) =>
+          groupedOrder.product_id === order.product_id &&
+          groupedOrder.vendor_name === order.vendor_name
       );
       if (existingOrder) {
         existingOrder.total_count += order.item_count;
@@ -33,9 +34,70 @@ function App() {
       }
       return acc;
     }, []);
-    
+
     return groupedOrders;
   };
+
+  const generateChartData = (groupedOrders) => {
+    const labels = [];
+    const data = [];
+
+    groupedOrders.forEach((order) => {
+      const label = `${order.month}-${order.year}`;
+      if (!labels.includes(label)) {
+        labels.push(label);
+        data.push(order.total_count);
+      }
+    });
+
+    const dataset = {
+      label: 'Total Item Count',
+      data: data,
+      fill: false,
+      borderColor: 'blue',
+      tension: 0.4
+    };
+
+    return {
+      labels: labels,
+      datasets: [dataset]
+    };
+  };
+
+  const fetchOrders = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/orders?vendorName=${vendorName}`);
+      const modifiedOrders = response.data.map((order) => ({
+        ...order
+      }));
+      const groupedOrders = groupOrders(modifiedOrders);
+      setOrders(groupedOrders);
+
+      const chartData = generateChartData(groupedOrders);
+      setChartData(chartData);
+
+      const chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      };
+      setChartOptions(chartOptions);
+
+      setChartOptions(chartOptions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (vendorName !== '') {
+      fetchOrders();
+    }
+  }, [vendorName]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -45,27 +107,31 @@ function App() {
   return (
     <div className="w-100 vh-10 d-flex justify-content-center align-items-center">
       <div className="w-150">
+        {chartData && (
+          <div>
+            <Line data={chartData} options={chartOptions} />
+          </div>
+        )}
         <form onSubmit={handleSubmit}>
           <input type="text" value={vendorName} onChange={(e) => setVendorName(e.target.value)} placeholder="Enter Vendor Name" />
-          <button type="submit">Search</button>
+          <button type="submit">Fetch Orders</button>
         </form>
-        <br />
-        <table style={{ border: "1px solid black" }}> {/* Add inline styles with the border property */}
+        <table style={{ border: '1px solid black' }}>
           <thead>
             <tr>
-              <th style={{ border: "1px solid black" }}>Product ID</th>
-              <th style={{ border: "1px solid black" }}>Parent Product Name</th>
-              <th style={{ border: "1px solid black" }}>Vendor Name</th>
-              <th style={{ border: "1px solid black" }}>Total Count</th>
+              <th style={{ border: '1px solid black' }}>Product ID</th>
+              <th style={{ border: '1px solid black' }}>Parent Product Name</th>
+              <th style={{ border: '1px solid black' }}>Vendor Name</th>
+              <th style={{ border: '1px solid black' }}>Total Count</th>
             </tr>
           </thead>
           <tbody>
             {orders.map((order) => (
               <tr key={order._id}>
-                <td style={{ border: "1px solid black" }}>{order.product_id}</td>
-                <td style={{ border: "1px solid black" }}>{order.parent_product_name}</td>
-                <td style={{ border: "1px solid black" }}>{order.vendor_name}</td>
-                <td style={{ border: "1px solid black" }}>{order.total_count}</td>
+                <td style={{ border: '1px solid black' }}>{order.product_id}</td>
+                <td style={{ border: '1px solid black' }}>{order.parent_product_name}</td>
+                <td style={{ border: '1px solid black' }}>{order.vendor_name}</td>
+                <td style={{ border: '1px solid black' }}>{order.total_count}</td>
               </tr>
             ))}
           </tbody>
